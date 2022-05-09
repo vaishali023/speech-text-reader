1cdcd522017b955d804b48bb02b314aff06081a7
vaishali023 <vaishalichaudhary011@gmail.com>
2022-05-09 14:29:52 -0400

    done with css and speech voices in script

---
+++ script.js
@@ -0,0 +1,102 @@
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+const data = [
+    {
+        image: './images/drink.jpg',
+        text: "I am thirsty"
+    },
+    {
+        image: './images/food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: './images/tired.jpg',
+        text: "I'm Tired"
+    },
+    {
+        image: './images/hurt.jpg',
+        text: "I'm Hurt"
+    },
+    {
+        image: './images/happy.jpg',
+        text: "I'm Happy"
+     },
+    {
+        image: './images/angry.jpg',
+        text: "I'm Angry"
+    },
+    {
+        image: './images/sad.jpg',
+        text: "I'm Sad"
+      },
+      {
+        image: './images/scared.jpg',
+        text: "I'm Scared"
+      },
+      {
+        image: './images/outside.jpg',
+        text: 'I Want To Go Outside'
+      },
+      {
+        image: './images/home.jpg',
+        text: 'I Want To Go Home'
+      },
+      {
+        image: './images/school.jpg',
+        text: 'I Want To Go To School'
+      },
+      {
+        image: './images/grandma.jpg',
+        text: 'I Want To Go To Grandmas'
+      }
+];
+
+data.forEach(createBox);
+
+//Create speech boxes
+function createBox(item) {
+    const box = document.createElement('div');
+    const { image, text} = item;
+    box.classList.add('box');
+
+    box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+   
+  // @todo - speak event
+  main.appendChild(box);
+
+}
+
+//Store voices
+let voices = [];
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+    
+        voicesSelect.appendChild(option);
+    });
+}
+
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+//Toggle text box
+toggleBtn.addEventListener('click', () => 
+   document.getElementById('text-box').classList.toggle('show')
+);
+
+//Close button
+closeBtn.addEventListener('click', () => 
+   document.getElementById('text-box').classList.remove('show')
+);
